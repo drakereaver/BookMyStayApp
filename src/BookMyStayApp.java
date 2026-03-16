098f7b0af297cb75b7a0f6e51fd5589e3e6eacc8
drakereaver <mohammedayaan8240@gmail.com>
2026-03-16 15:00:34 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,16 @@
+/**
+ * Use Case 1
+ * @author Developer
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+public class BookMyStayApp {
+    public class BookMyStayApp {
+        public static void main(String[] args) {
+            System.out.println("Welcome to the Hotel Booking Management System");
+            System.out.println("System initialized successfully");
+        }
+    }
+}
